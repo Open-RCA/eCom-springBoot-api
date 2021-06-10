e60d4989f8e13840d6aef826f7969a2bc990685c
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-10 09:38:20 +0200

    first-of-first

---
+++ src/test/java/com/openrca/templates/ecomspringboot/EcomSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openrca.templates.ecomspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
